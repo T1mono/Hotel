ebd11280f42c30db3a069716304c91a2cd02bda0
T1mon <godropopka@gmail.com>
2025-06-04 16:58:29 +0500

    Added bookRoom() and calculatePrice()

--- src/main/java/ru/javadaddy/service/BookingService.java
+++ src/main/java/ru/javadaddy/service/BookingService.java
@@ -4,6 +4,8 @@ import ru.javadaddy.model.Room;
 import ru.javadaddy.repository.RoomRepository;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookingService {
@@ -41,13 +43,48 @@ public class BookingService {
         return true;
     }
 
+    //TODO: 1. Проверить даты бронирования и если они свободны забронируй
     public boolean bookRoom(Room room, LocalDate checkIn, LocalDate checkOut) {
         // TODO: Реализовать логику бронирования номера
-        return false;
+        if (!isRoomAvailable(room, checkIn, checkOut)) {
+            return false;
+        }
+
+        List<LocalDate> datesByRoomId = roomRepository.findDatesByRoomId(room.id());
+        List<LocalDate> updateBookedDates = new ArrayList<>(datesByRoomId);
+        updateBookedDates.addAll(datesByRoomId);
+
+        LocalDate date = checkIn;
+        while (date.isBefore(checkOut)) {
+            updateBookedDates.add(date);
+            date = date.plusDays(1);
+        }
+
+        roomRepository.saveNewReservation(room.id(), updateBookedDates);
+        return true;
     }
 
     public double calculatePrice(Room room, LocalDate checkIn, LocalDate checkOut) {
         // TODO: Реализовать расчет стоимости
-        return 0.0;
+
+        if (room == null || checkIn == null || checkOut == null) {
+            throw new IllegalArgumentException("room, checkIn, checkOut не могут быть null");
+        }
+
+        if (checkIn.isAfter(checkOut)) {
+            throw new IllegalArgumentException("Дата заезда не может быть позже даты выезда");
+        }
+
+        //Получаем цену за номер
+        double price = room.pricePerNight();
+
+        //Разница дней между checkIn и checkOut
+        long night = ChronoUnit.DAYS.between(checkIn, checkOut);
+
+        if (night < 1) {
+            throw new IllegalArgumentException("Минимальное бронирование - 1 ночь");
+        }
+
+        return price * (double) night;
     }
 }
---
+++ src/test/java/ru/javadaddy/service/BookingServiceTest.java
@@ -0,0 +1,22 @@
+package ru.javadaddy.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+class BookingServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void isRoomAvailable() {
+    }
+
+    @Test
+    void bookRoom() {
+    }
+
+    @Test
+    void calculatePrice() {
+    }
+}
